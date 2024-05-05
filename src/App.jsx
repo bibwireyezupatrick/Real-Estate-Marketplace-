dd9b26481a5ab7db6dafc1cab309211b78910793
bibwireyezupatrick <bibwireyezupatrick@gmail.com>
2024-05-06 03:18:46 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUP";
+import About from "./pages/About";
+
+
+const App = () => {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      
+    </Routes>
+  </BrowserRouter>
+    
+  
 }
+
+export default App
